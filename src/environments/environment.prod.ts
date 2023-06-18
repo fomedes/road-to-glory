86a795fde428cbaba0cc4409528a8cbe2ba31b77
[Francisco Omedes] <[fomedes@uoc.edu]>
2023-06-18 21:25:00 +0100

    api endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: '',
+  apiBaseUrl:
+    'postgres://ppiytqdndqivjs:99c67fe2960ed6869f61a5609e9da863d04786e239be3f064ae588c70a958103@ec2-3-232-218-211.compute-1.amazonaws.com:5432/d439lo9q5b3hrv',
 };
